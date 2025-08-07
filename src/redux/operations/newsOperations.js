462568b7aeedc4fdf490fbe1d461f828a028e971
Urban <urban.mutagen@gmail.com>
2025-08-07 20:01:00 +0300

    news add redux and newsPage NewsList and search need little bit styles

---
+++ src/redux/operations/newsOperations.js
@@ -0,0 +1,15 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { fetchNews } from "../../../API/API";
+
+export const fetchNewsThunk = createAsyncThunk(
+  "news/fetchNews",
+  async ({ keyword, page, limit }, { rejectWithValue }) => {
+    try {
+      const data = await fetchNews({ keyword, page, limit });
+      console.log("Data:", data);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
---
+++ src/redux/selectors/newsSelectors.js
@@ -0,0 +1,6 @@
+export const selectNews = (state) => state.news.news;
+export const selectPage = (state) => state.news.page;
+export const selectPerPage = (state) => state.news.perPage;
+export const selectTotalPages = (state) => state.news.totalPages;
+export const selectNewsStatus = (state) => state.news.status;
+export const selectNewsError = (state) => state.news.error;
