60ef4481029f11a4d7ab23a611bb067fb703c1f5
Urban <urban.mutagen@gmail.com>
2025-08-13 19:49:31 +0300

    home section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,33 +1,67 @@
-// import Container from "../../components/common/Container/Container";
-
-// const HomePage = () => {
-//   return (
-//     <section>
-//       <Container>
-//         <h1 className="text-[200px]">HOME</h1>
-//       </Container>
-//     </section>
-//   );
-// };
-
-// export default HomePage;
+import clsx from "clsx";
 import Container from "../../components/common/Container/Container";
 
 const HomePage = () => {
   return (
     <section>
       <Container type="common">
-        <div className="flex flex-col">
-          <div className="flex flex-row items-end justify-center px-[32px] pb-[32px] pt-[112px]">
-            <h1>
-              Take good <span>care</span> of your small pets
+        <div className="mb-[10px] flex flex-col md:mb-[16px]">
+          <div
+            className={clsx(
+              "flex flex-col items-end justify-center rounded-b-[30px] bg-[--accent-orange] px-[20px] pb-[50px] pt-[60px]",
+              "md:px-[32px] md:pb-[44px] md:pt-[112px]",
+              "lg:flex-row lg:justify-between lg:px-[64px] lg:pb-[32px]",
+            )}
+          >
+            <h1
+              className={clsx(
+                "mb-[24px] text-[50px] font-bold leading-[96%] tracking-[-0.03em] text-[--light-white]",
+                "md:mb-[32px] md:text-[80px]",
+                "lg:mb-0 lg:w-[760px] lg:text-[90px] lg:leading-[97%]",
+              )}
+            >
+              Take good <span className="text-[--superlight-white]">care</span>{" "}
+              of your small pets
             </h1>
-            <p>
+            <p
+              className={clsx(
+                "text-[14px] font-medium leading-[129%] tracking-[-0.02em] text-[--light-white]",
+                "md:w-[255px] md:text-[18px] md:leading-[122%]",
+                "",
+              )}
+            >
               Choosing a pet for your home is a choice that is meant to enrich
               your life with immeasurable joy and tenderness.
             </p>
           </div>
-          <img src="" alt="" />
+          <picture className="w-full max-w-full overflow-hidden rounded-[30px] bg-amber-500">
+            <source
+              media="(min-width: 1280px)"
+              srcSet="
+                /public/common/desktop/home-dog-desktop@1x.png 1x,
+                /public/common/desktop/home-dog-desktop@2x.png 2x"
+              type="image/png"
+            />
+            <source
+              media="(min-width: 768px) and (max-width: 1279px)"
+              srcSet="
+                /public/common/laptop/home-dog-laptop@1x.png 1x,
+                /public/common/laptop/home-dog-laptop@2x.png 2x"
+              type="image/png"
+            />
+            <source
+              media="(max-width: 767px)"
+              srcSet="
+                /public/common/phone/home-dog-phone@1x.png 1x,
+                /public/common/phone/home-dog-phone@2x.png 2x"
+              type="image/png"
+            />
+            <img
+              src="/public/common/phone/home-dog-phone@1x.png"
+              alt="Small pets care"
+              className="h-auto w-full object-cover"
+            />
+          </picture>
         </div>
       </Container>
     </section>
