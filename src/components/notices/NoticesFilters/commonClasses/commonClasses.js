9f2e710c018c05ea9fb2bcc9c33c871d4d65ccac
sh1xzyy <alekslungol@gmail.com>
2025-08-24 00:13:51 +0300

    radio buttons logic + styles

--- src/components/notices/NoticesFilters/commonClasses/commonClasses.js
+++ src/components/notices/NoticesFilters/commonClasses/commonClasses.js
@@ -1,2 +1,2 @@
 export const radioBtnClass =
-  "inline-flex h-[42px] cursor-pointer items-center justify-center rounded-[30px] bg-light-white px-[12px] text-[14px] font-medium leading-[1.28] tracking-[-0.03em] text-gray-900 md:h-[48px] md:text-[16px] md:leading-[1.25] md:px-[14px]";
+  "inline-flex h-[42px] items-center justify-center rounded-[30px] px-[12px] md:h-[48px] md:px-[14px] transition-all duration-[250ms]";
